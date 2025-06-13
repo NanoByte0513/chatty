a36bd6d59d4581afc9ff7efbd77121e01c652d19
NanoByte0513 <qn20081220@gmail.com>
2025-06-13 15:14:06 +0800

    Add some CMakeLists files

--- scripts/linux_cuda.sh
+++ scripts/linux_cuda.sh
@@ -1,5 +1,16 @@
-rm -r ./build
+pushd ./
+rm -rf ./build
 mkdir build && cd build
 cmake ../src -DCHATTY_CUDA_ENABLED=ON \
+             -DCHATTY_x86_ENABLED=OFF \
+             -DCHATTY_STATIC_LIB=OFF \
              -DCHATTY_USE_INT8=ON
 make -j8
+popd
+
+pushd ./
+rm -rf ./build_test
+mkdir build_test && cd build_test
+cmake ../src/test -DTEST_CUDA_ENABLED=ON
+make -j8
+popd
